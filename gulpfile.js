b13a0133ab1257b5e02e60284a5dfa987891adc4
Cedric <Cedric@DESKTOP-BTF3HDL>
2018-02-08 14:23:55 +0100

    gulpfile.js, package.json update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require('gulp');
+var del = require('del');
+var minifyCSS = require('gulp-minify-css');
+var notify = require('gulp-notify');
+var rename = require('gulp-rename');
+var sass = require('gulp-ruby-sass');
+var csslint = require('gulp-csslint');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+            .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('styles', function() {
+    return sass('src/scss/*.scss', { style: 'expand' })
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(minifyCSS())
+            .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('css', function() {
+  return gulp.src('dist/css/*.css')
+    .pipe(csslint())
+    .pipe(csslint.formatter());
+});
+
+gulp.task('clean', function(){
+  return del(['dist/css/**/*', 'dist/js/**/*']);
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('scripts', 'styles', 'css');
 });
